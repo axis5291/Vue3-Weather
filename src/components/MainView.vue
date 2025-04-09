b66f369eb02388b0f1f8b546ab4f8453fdac8253
Axis <erlia@naver.com>
2025-04-10 05:21:04 +0900

    9.3시간별 예보정보 반영

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -37,17 +37,15 @@
                 <p>이번주 날씨보기</p>
             </div>
             <div class="timelyWeatherBox">
-                <div class="timelyWeather">
+                <div class="timelyWeather" v-for="(data, index) in threeHourForecastData" :key="index">
                     <div class="icon">
-                        <img  :src="`/images/${icon}.png`" alt="weather icon" />
-
-                     
+                        <img  :src="`/images/${data.icon}.png`" alt="weather icon" />
                     </div>
                     <div class="data">
-                        <p class="time">2pm </p>
-                        <p class="currentDegree"> {{temp}} &deg;</p>
+                        <p class="time">{{ data.time }}</p>
+                        <p class="currentDegree"> {{data.temp}} &deg;</p>
                         <div>
-                          <img src="../assets/drop.png" alt="" />
+                          <img src="/images/drop.png" alt="" />
                           <p class="fall"> 15% </p>
                         </div>
                     </div> 
@@ -71,18 +69,17 @@
     dayjs.locale('ko'); // 한국어로 설정
 
     const temporaryData =reactive({
-        humidity: { title: '습도', value: '60%' },
-        wind: { title: '풍속', value: '5.0m/s' },
-        pressure: { title: '체감온도', value: '1013hPa' },
-       },
-    );
+                        humidity: { title: '습도', value: '60%' },
+                        wind: { title: '풍속', value: '5.0m/s' },
+                        pressure: { title: '체감온도', value: '1013hPa' },
+       },);
        
 
     //https://openweathermap.org/사이트에서 날씨정보를 가져오는 API
     // 상단에 API탭을 클릭 후, Free Access로 제공하는 것을 찾아  API call로 주소를 찾고 
     // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}형태로 되어 있는 것을 아래에 맞게 수정해서 사용한다.
     //지금은 current weather API를 사용하고 있음
-    const API_KEY = 'eab0698d3bb063e356afe1f39500bd90';
+    const API_KEY = '284bfdeb630520653864189833ba7c68';
     const initialLat = 37.5683;   //위도(Latitude:læt.ɪ.tjuːd)
     const initialLon = 126.9778;  //경도(Longitude:ˈlɒŋ.ɡɪ.tjuːd)   서울을 가리킴
 
@@ -92,9 +89,10 @@
     const icon=ref('');
     const cityName=ref('');
 
+    //현재 날씨 정보를 가져오는 API
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${initialLat}&lon=${initialLon}&appid=${API_KEY}`)
     .then((response)=>{
-            console.log(response.data);  //**API에서 받아온 데이터을 콘솔에 찍어보고 넘어온 데이터를 살펴보면서 아래에 필요한 데이터를 사용한다.
+            console.log('현재날씨 정보',response.data);  //**API에서 받아온 데이터을 콘솔에 찍어보고 넘어온 데이터를 살펴보면서 아래에 필요한 데이터를 사용한다.
        
             cityName.value = response.data.name;
             currentTemp.value= (response.data.main.temp - 273.15).toFixed(1); // 섭씨 변환
@@ -102,19 +100,33 @@
             temporaryData.humidity.value = response.data.main.humidity + '%';//습도
             temporaryData.pressure.value = (response.data.main.feels_like - 273.15).toFixed(1) + '°C';  //체감온도
             temporaryData.wind.value = response.data.wind.speed + 'm/s';  //풍속
-           
-
-           
-           
-
-        }
+           }
     ).catch(
         (error)=>{
             console.log(error);
         }
     );
 
-   
+//3시간 간격 예보정보를 가져오는 API
+    const threeHourForecastData=ref([]);
+       
+    axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${initialLat}&lon=${initialLon}&appid=${API_KEY}`)
+    .then((response) => {
+    console.log('3시간 일기얘보 전체정보:', response.data);
+
+    for(let i=0; i<response.data.list.length; i++){
+        const time = dayjs(response.data.list[i].dt_txt).format('MM/DD HH:mm');  
+        const temp = (response.data.list[i].main.temp - 273.15).toFixed(1);
+        const icon = response.data.list[i].weather[0].icon;
+        threeHourForecastData.value.push({time, temp, icon});
+    }
+
+    console.log('3시간얘보 정보 시간, 온도, 아이콘:', threeHourForecastData.value);
+
+  })
+  .catch((error) => {
+    console.error('에러 발생:', error);
+  });
   
 </script>
 
