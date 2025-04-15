54942dacd716c06384ef4dc9654415600adc0820
Axis <erlia@naver.com>
2025-04-15 12:11:53 +0900

    15.pinia 설정

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
-import {createPinia} from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './routes/index.js'
+import { pinia } from './store/index.js'
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 //fontawesome을 사용하기 위해 설치함.
 //npm install @fortawesome/fontawesome-free 실행하면 node_modules/@fortawesome/fontawesome-free 폴더에 설치됨.
 //이후 main.js에서 import '@fortawesome/fontawesome-free/css/all.min.css'를 통해 css파일을 불러옴.
 
-createApp(App).use(router).use(createPinia()).mount('#app');
+createApp(App).use(router).use(pinia).mount('#app');
---
+++ src/store/openWeatherApi.js
@@ -0,0 +1,12 @@
+import {defineStore} from 'pinia';
+import axios from 'axios';
+
+export const useOpenWeatherApiStore = defineStore('openWeatherApi', {
+    state:()=>(),
+
+    actions: {
+    
+    }
+
+)
+
